622556026f98226316a2d5d00e745907d24269de
Anuar <anuarakimbekov@Anuar.local>
2025-12-31 00:41:29 +0500

    Fix: исправлены отступы и структура layout на страницах search и favorites

--- frontend/src/app/favorites/page.tsx
+++ frontend/src/app/favorites/page.tsx
@@ -32,17 +32,17 @@ const FavoritesPage = () => {
 
   return (
     <div className="bg-black min-h-screen flex flex-col">
-      {/* Navbar - separate zone with Header */}
-      <div className="bg-white">
+      {/* Navbar - separate zone with BRANDLIST */}
+      <div className="bg-white pt-12 pb-8">
         <div className="max-w-[calc(100vw-64px)] mx-auto px-4 sm:px-6 md:px-8">
           <Header />
         </div>
       </div>
 
       {/* Main content block - card container */}
-      <div className="flex-1 bg-white">
+      <div className="flex-1 flex flex-col bg-white" style={{ marginTop: '-1px' }}>
         <div 
-          className="mx-4 sm:mx-6 md:mx-8"
+          className="mx-4 sm:mx-6 md:mx-8 flex-1 flex flex-col"
           style={{
             backgroundColor: '#f3f0e9',
             boxShadow: 'inset 0 0 2px rgba(0, 0, 0, 0.02)',
--- frontend/src/app/search/[query]/page.tsx
+++ frontend/src/app/search/[query]/page.tsx
@@ -94,17 +94,17 @@ const SearchResults = ({ params }: SearchResultsProps) => {
 
   return (
     <div className="bg-black min-h-screen flex flex-col">
-      {/* Navbar - separate zone with Header */}
-      <div className="bg-white">
+      {/* Navbar - separate zone with BRANDLIST */}
+      <div className="bg-white pt-12 pb-8">
         <div className="max-w-[calc(100vw-64px)] mx-auto px-4 sm:px-6 md:px-8">
           <Header />
         </div>
       </div>
 
       {/* Main content block - card container */}
-      <div className="flex-1 bg-white">
+      <div className="flex-1 flex flex-col bg-white" style={{ marginTop: '-1px' }}>
         <div 
-          className="mx-4 sm:mx-6 md:mx-8"
+          className="mx-4 sm:mx-6 md:mx-8 flex-1 flex flex-col"
           style={{
             backgroundColor: '#f3f0e9',
             boxShadow: 'inset 0 0 2px rgba(0, 0, 0, 0.02)',
@@ -112,7 +112,7 @@ const SearchResults = ({ params }: SearchResultsProps) => {
             borderTopRightRadius: '1.5rem',
           }}
         >
-          <div className="px-4 sm:px-8 md:px-12 lg:px-16 py-8 sm:py-10 md:py-12">
+          <div className="px-4 sm:px-8 md:px-12 lg:px-16 py-8 sm:py-10 md:py-12 flex flex-col flex-1">
             <div className="py-4">
               <SearchBar
                 initialQuery={query}
